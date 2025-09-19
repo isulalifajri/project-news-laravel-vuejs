a03837c75044904b69d8357e2882ceb3e22e714c
isulalifajri@gmail.com <isulalifajri@gmail.com>
2025-09-19 19:19:59 +0700

    fix crud role permission

--- app/Filament/Resources/RoleResource/Pages/CreateRole.php
+++ app/Filament/Resources/RoleResource/Pages/CreateRole.php
@@ -9,4 +9,25 @@ use Filament\Resources\Pages\CreateRecord;
 class CreateRole extends CreateRecord
 {
     protected static string $resource = RoleResource::class;
+
+    protected function mutateFormDataBeforeCreate(array $data): array
+    {
+        // hapus semua permissions_* supaya tidak dimasukkan ke tabel roles
+        $data = collect($data)
+            ->reject(fn($v, $k) => str_starts_with($k, 'permissions_'))
+            ->toArray();
+
+        return $data;
+    }
+
+    protected function afterCreate(): void
+    {
+        $ids = collect($this->form->getState())
+                ->filter(fn ($v, $k) => str_starts_with($k, 'permissions_'))
+                ->flatten()
+                ->filter()
+                ->toArray();
+
+            $this->record->permissions()->sync($ids);
+    }
 }
